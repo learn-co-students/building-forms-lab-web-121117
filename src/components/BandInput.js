5c60dc6d53345cd1a1fec876138ef9e41ca0c003
github username <github email address>
2018-03-11 16:13:30 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+  state = {
+    text:''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    },
+    ()=>{console.log(this.state)})
+  }
+
+  handleSubmit = (event) => {
+
+    event.preventDefault()
+    this.props.store.dispatch({
+      type: 'ADD_BAND',
+      band: this.state.text
+    })
+
+  }
+
   render() {
     return (
       <div>
-        bands component
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' value={this.state.text} onChange={this.handleChange}></input>
+          <input type='submit'></input>
+        </form>
       </div>
     );
   }
 };
 
 export default BandInput;
+
+// 1. You will need to write the `BandInput` component. The component should have a form with a text input and submit button. We would like to update the *component's* state each time the user types a new character in the input. However, we only update the store's state when the user hits the submit button.
+//   * Note: To have access to the store's dispatch method, we pass through the entire store as a prop.
--- src/components/Bands.js
+++ src/components/Bands.js
@@ -4,10 +4,15 @@ class Bands extends Component {
   render(){
     return (
       <div>
-        band input
+        {this.props.store.getState().bands.map(band=>{
+          return <li>{band}</li>
+        })}
       </div>
     );
   }
 };
 
 export default Bands;
+
+
+// 2. You will need to write the `Bands` component. This component should display a ul with a list element for each of the bands in the store's state. Each list element should have text with the corresponding name of the band.
